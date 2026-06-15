693272c6f66c70b75aef08cf0503429ca51bbc06
chandu078 <chandudyavanapelli03@gmail.com>
2026-06-15 19:32:57 +0000

    fairlady: Update from COS 11.A.31
    Change-Id: Ib3666cbc286aa7867c7625da1879d5b0ac6f4262

--- lineage_fairlady.mk
+++ lineage_fairlady.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := PLZ110
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BP2A.250605.015 1778692547865 release-keys" \
-    BuildFingerprint=OnePlus/PLZ110/OP64DDL1:16/BP2A.250605.015/B.3a6dfa9-1699340-169933c:user/release-keys \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1779913686989 release-keys" \
+    BuildFingerprint=OnePlus/PLZ110/OP64DDL1:16/BP2A.250605.015/B.4ff17bb-2be9a75-2be9a74:user/release-keys \
     DeviceName=OP64DDL1 \
     DeviceProduct=PLZ110 \
     SystemDevice=OP64DDL1 \
